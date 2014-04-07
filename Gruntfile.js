e58f7c5287df58044ed5868a246ac84125dd0229
Samy Pessé <samypesse@gmail.com>
2014-04-07 10:33:03 -0700

    Use grunt-http-server for testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-gitbook');
     grunt.loadNpmTasks('grunt-gh-pages');
     grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-http-server');
 
     grunt.initConfig({
         'gitbook': {
@@ -25,19 +25,27 @@ module.exports = function (grunt) {
         'clean': {
             files: '.grunt'
         },
-        'connect': {
-            server: {
-                options: {
-                    port: 4000,
-                    base: '.grunt/gitbook'
-                }
+        'http-server': {
+            'dev': {
+                // the server root directory
+                root: '.grunt/gitbook',
+
+                port: 4000,
+                host: "127.0.0.1",
+
+                showDir : true,
+                autoIndex: true,
+                defaultExt: "html",
+
+                //wait or not for the process to finish
+                runInBackground: false
             }
         }
     });
 
     grunt.registerTask('test', [
         'gitbook',
-        'connect'
+        'http-server'
     ]);
     grunt.registerTask('publish', [
         'gitbook',
